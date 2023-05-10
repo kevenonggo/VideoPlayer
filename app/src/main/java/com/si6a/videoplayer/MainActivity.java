638b5d0974098acc699862dcafaa8461e54e06a0
Keven Mandari Onggo <kevenven26@mhs.mdp.ac.id>
2023-05-10 22:32:29 +0700

    Finish VideoPlayer
    (Force Close)

--- app/src/main/java/com/si6a/videoplayer/MainActivity.java
+++ app/src/main/java/com/si6a/videoplayer/MainActivity.java
@@ -1,14 +1,89 @@
 package com.si6a.videoplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements VideoRVAdapter.VideoClickInterface{
+
+    private RecyclerView videoRV;
+    private ArrayList<VideoRVModal> videoRVModalArrayList;
+    private VideoRVAdapter videoRVAdapter;
+    private static final int STORAGE_PERMISSION = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoRV = findViewById(R.id.rv_video);
+        videoRVModalArrayList = new ArrayList<>();
+        videoRVAdapter = new VideoRVAdapter(videoRVModalArrayList,this, this::onVideoClick);
+        videoRV.setLayoutManager(new GridLayoutManager(this,2));
+        videoRV.setAdapter(videoRVAdapter);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_PERMISSION);
+        }else {
+            getVideos();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==STORAGE_PERMISSION){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Pemission Granted", Toast.LENGTH_SHORT).show();
+                getVideos();
+            }else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private void getVideos(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+        if (cursor!=null && cursor.moveToFirst()){
+            do{
+                @SuppressLint("Range") String videoTitle = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE));
+                @SuppressLint("Range") String videoPath = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                Bitmap videoThumbnail = ThumbnailUtils.createVideoThumbnail(videoPath, MediaStore.Images.Thumbnails.MINI_KIND);
+
+                videoRVModalArrayList.add(new VideoRVModal(videoTitle, videoPath, videoThumbnail));
+            }while (cursor.moveToNext());
+        }
+        videoRVAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onVideoClick(int position) {
+        Intent intent = new Intent(MainActivity.this, VideoPlayerActivity.class);
+        intent.putExtra("videoName", videoRVModalArrayList.get(position).getVideoName());
+        intent.putExtra("videoPath", videoRVModalArrayList.get(position).getVideoPath());
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/si6a/videoplayer/VideoRVAdapter.java
@@ -0,0 +1,63 @@
+package com.si6a.videoplayer;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class VideoRVAdapter extends RecyclerView.Adapter<VideoRVAdapter.ViewHolder> {
+
+    private ArrayList<VideoRVModal> videoRVModalArrayList;
+    private Context context;
+    private VideoClickInterface videoClickInterface;
+
+    public VideoRVAdapter(ArrayList<VideoRVModal> videoRVModalArrayList, Context context, VideoClickInterface videoClickInterface) {
+        this.videoRVModalArrayList = videoRVModalArrayList;
+        this.context = context;
+        this.videoClickInterface = videoClickInterface;
+    }
+
+    @NonNull
+    @Override
+    public VideoRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.video_rv_item,parent,false);
+       return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoRVAdapter.ViewHolder holder, int position) {
+        VideoRVModal videoRVModal = videoRVModalArrayList.get(position);
+        holder.thumbnailIV.setImageBitmap(videoRVModal.getThumbNail());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoClickInterface.onVideoClick(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoRVModalArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView thumbnailIV;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            thumbnailIV = itemView.findViewById(R.id.iv_thumbnail);
+        }
+    }
+
+    public interface VideoClickInterface{
+        void onVideoClick(int position);
+    }
+}
